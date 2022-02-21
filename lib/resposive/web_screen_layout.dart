f4e366b4857530ddde413930ceb8404c96360ac6
Lucky723 <lakshya258@gmail.com>
2022-02-21 13:51:58 +0530

    responsive mobile and web ui

--- lib/resposive/web_screen_layout.dart
+++ lib/resposive/web_screen_layout.dart
@@ -2,54 +2,89 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import '../utils/colours.dart';
+import '../widgets/search.dart';
+import '../widgets/search_buttons.dart';
+import '../widgets/translation_buttons.dart';
+import '../widgets/web/web_footer.dart';
+
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
-           backgroundColor: backgroundColor,
-          elevation: 0,
-          actions: [
-            TextButton(
-              onPressed: () {},
-              child: const Text(
-                'Gmail',
-                style: TextStyle(
-                  color: primaryColor,
-                  fontWeight: FontWeight.w400,
-                ),
+        backgroundColor: backgroundColor,
+        elevation: 0,
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Gmail',
+              style: TextStyle(
+                color: primaryColor,
+                fontWeight: FontWeight.w400,
               ),
             ),
-            TextButton(
-              onPressed: () {},
-              child: const Text(
-                'Images',
-                style: TextStyle(
-                  color: primaryColor,
-                  fontWeight: FontWeight.w400,
-                ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Images',
+              style: TextStyle(
+                color: primaryColor,
+                fontWeight: FontWeight.w400,
               ),
             ),
-           const SizedBox(width: 10),
-            IconButton(
-                icon: SvgPicture.asset('assets/images/more-apps.svg',
-                    color: primaryColor),
-                onPressed: () {}),
-            const SizedBox(width: 10),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10).copyWith(right: 10,),
-              child: MaterialButton(
-                onPressed: () {},
-                color: const Color(0xff1A73EB),
-                child: const Text(
-                  'Sign in',
-                  style: TextStyle(color: Colors.white),
-                ),
+          ),
+          const SizedBox(width: 10),
+          IconButton(
+              icon: SvgPicture.asset('assets/images/more-apps.svg',
+                  color: primaryColor),
+              onPressed: () {}),
+          const SizedBox(width: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10).copyWith(
+              right: 10,
+            ),
+            child: MaterialButton(
+              onPressed: () {},
+              color: const Color(0xff1A73EB),
+              child: const Text(
+                'Sign in',
+                style: TextStyle(color: Colors.white),
               ),
-            )
+            ),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 5, right: 5),
+        child: Column(
+          children: [
+            SizedBox(height: size.height * 0.25),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // since children of column are not supposed to be 'spaced between'
+                  Column(
+                    children: const [
+                      Search(),
+                      SizedBox(height: 20),
+                      SearchButtons(),
+                      SizedBox(height: 20),
+                      TranslationButtons(),
+                    ],
+                  ),
+                  const WebFooter(),
+                ],
+              ),
+            ),
           ],
+        ),
       ),
     );
   }
